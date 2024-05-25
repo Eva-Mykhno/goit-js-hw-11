d864f4c2c5ccbf9cf973b4cb36b4f9b2c1d960df
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-05-25 15:17:34 +0300

    render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,39 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export function renderPhotos(images) {}
+const gallery = document.querySelector('.gallery');
+
+function templatePhotos(images) {
+  return images
+    .map(image => {
+      return `<li class="gallery-item">
+  <a class="gallery-link" href="${image.largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${image.webformatURL}"
+      data-source="${image.largeImageURL}"
+      alt="${image.tags}"
+    />
+  </a>
+  <div>
+  <p class="info-image">${image.likes}</p>
+  <p class="info-image">${image.views}</p>
+  <p class="info-image">${image.comments}</p>
+  <p class="info-image">${image.downloads}</p>
+  </div>
+</li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.8,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function renderPhotos(images) {
+  const markUp = templatePhotos(images);
+  gallery.insertAdjacentHTML('afterbegin', markUp);
+  lightbox.refresh();
+}
